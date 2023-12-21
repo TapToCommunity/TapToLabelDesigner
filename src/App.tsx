b34b38c5d080a937ef78305f7f57139d941d9c5f
Andrea Bogazzi <abogazzi@shutterstock.com>
2023-12-21 02:01:08 +0100

    changed title emptied app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import './App.css'
 
 function App() {
   return (
+    <>
       <h1>Start by dropping in the browser one or more images you want to use on your labels</h1>
+      <h4>(Nothing works, i m building this as you read)</h4>
+    </>
   )
 }
 
