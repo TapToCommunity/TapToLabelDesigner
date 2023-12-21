ee465bc771d46c0d54fe4a6c0e27cd8fd2aaa09d
Andrea Bogazzi <abogazzi@shutterstock.com>
2023-12-21 10:30:28 +0100

    added drop ability

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import { useContext } from 'react';
+import { FileDropContext } from './components/FileDropper';
 
 function App() {
+  const files = useContext(FileDropContext);
+  const hasFiles = files.length > 0;
+  
+  if (!hasFiles) {
+    return (
+      <>
+        <h1>Start by dropping in the browser one or more images you want to use on your labels</h1>
+        <h4>(Nothing works, i m building this as you read)</h4>
+      </>
+    );
+  }
   return (
-    <>
-      <h1>Start by dropping in the browser one or more images you want to use on your labels</h1>
-      <h4>(Nothing works, i m building this as you read)</h4>
-    </>
+    files.map(file => file.name)
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { FileDropper } from './components/FileDropper';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <FileDropper>
+      <App />
+    </FileDropper>
   </React.StrictMode>,
 )
