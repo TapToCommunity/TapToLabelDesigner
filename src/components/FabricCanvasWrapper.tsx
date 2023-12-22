f24ad046ffcc9738459d70bc7af1c62dfb45db88
Andrea Bogazzi <abogazzi@shutterstock.com>
2023-12-22 02:06:07 +0100

    add images and canvases

--- src/components/FabricCanvasWrapper.tsx
+++ src/components/FabricCanvasWrapper.tsx
@@ -1,1 +1,59 @@
-// import { Canvas } from 'fabric';
+import { Canvas, FabricImage, util, Rect, FabricObject } from 'fabric';
+import { useRef, useEffect } from 'react';
+
+FabricObject.ownDefaults.originX = 'center';
+FabricObject.ownDefaults.originY = 'center';
+
+type WrapperProp = {
+  file: File;
+  setFabricCanvas: (canvas: Canvas | null) => void;
+}
+
+const cardLikeOptions = {
+  width: 854,
+  height: 539,
+  rx: 30,
+  ry: 30,
+  strokeWidth: 1,
+  stroke: 'black',
+  strokeUniform: true,
+  fill: 'white',
+};
+
+export const FabricCanvasWrapper = ({ file, setFabricCanvas }: WrapperProp) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const fabricCanvas = new Canvas(canvasRef.current, {
+        width: 855,
+        height: 540
+      });
+      const cardBorder = new Rect(cardLikeOptions);
+      cardBorder.canvas = fabricCanvas;
+      fabricCanvas.clipPath = cardBorder;
+      fabricCanvas.backgroundImage = cardBorder;
+      fabricCanvas.centerObject(cardBorder);
+      const imageUrl = URL.createObjectURL(file);
+      if (file) {
+        util.loadImage(imageUrl).then((image) => {
+          const fabricImage = new FabricImage(image);
+          const scale = util.findScaleToCover(fabricImage, fabricCanvas);
+          fabricImage.scaleX = scale;
+          fabricImage.scaleY = scale;
+          fabricCanvas.add(fabricImage);
+          fabricCanvas.centerObject(fabricImage);
+          setFabricCanvas(fabricCanvas);
+        });
+      }
+      return () => {
+        fabricCanvas.dispose();
+        setFabricCanvas(null);
+      };
+    }
+  }, [setFabricCanvas, file]);
+
+  return (
+    <canvas ref={canvasRef} />
+  );
+}
--- src/components/LabelEditor.tsx
+++ src/components/LabelEditor.tsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from 'react';
+import { FabricCanvasWrapper } from './FabricCanvasWrapper';
+import { Canvas } from 'fabric';
 
 type LabelEditorProps = {
   file: File;
 }
 
 export const LabelEditor = ({ file }: LabelEditorProps) => {
-  return file.name;
+  const [fabricCanvas, setFabricCanvas] = useState<Canvas | null>(null)
+  useEffect(() => {
+    if (fabricCanvas) {
+      console.log(fabricCanvas)
+    }
+  }, [fabricCanvas]);
+
+  return <FabricCanvasWrapper setFabricCanvas={setFabricCanvas} file={file} />;
 }
