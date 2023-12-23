e5be4ccb1d2508a72fa4b7502539fca17ff68ad1
Andrea Bogazzi <abogazzi@shutterstock.com>
2023-12-23 17:32:20 +0100

    added a dropdown for filters

--- src/components/FilterDropdown.tsx
+++ src/components/FilterDropdown.tsx
@@ -1,9 +1,12 @@
-import type { JSX, RefObject, ReactEventHandler } from 'react';
-import { useCallback } from 'react';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import { filters } from 'fabric';
-import type { Canvas, FabricImage } from 'fabric';
+import type { JSX, RefObject } from 'react';
+import { useCallback, useState } from 'react';
+import { classRegistry } from 'fabric';
+import type { Canvas, FabricImage, filters } from 'fabric';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 type FilterDropdownProps = {
   canvasArrayRef: RefObject<Canvas[]>;
@@ -11,22 +14,40 @@ type FilterDropdownProps = {
 
 const FilterDropdown = ({ canvasArrayRef }: FilterDropdownProps): JSX.Element => {
 
-  const toggleFilter = useCallback<ReactEventHandler<HTMLInputElement>>((evt) => {
-    const BWFilter = new filters.BlackWhite();
-    const filterOn = (evt.target as HTMLInputElement).checked;
+  const [filter, setCurrentFilter] = useState<string>('')
+
+  const toggleFilter = useCallback((evt: SelectChangeEvent<string>) => {
+    const value = evt.target.value;
+    const filter = value && new (classRegistry.getClass(value));
     const canvases = canvasArrayRef.current;
     if (canvases) {
       canvases.forEach((canvas) => {
         canvas.getObjects('image').forEach((image) => {
-          (image as unknown as FabricImage).filters = (filterOn ? [BWFilter as unknown as filters.BaseFilter] : []);
+          (image as unknown as FabricImage).filters = (filter ? [filter as filters.BaseFilter] : []);
           (image as FabricImage).applyFilters();
         });
-        canvas.renderAll();
+        canvas.requestRenderAll();
       });
+      setCurrentFilter(value);
     }
   }, []);
 
-  return <FormControlLabel control={<Checkbox onChange={toggleFilter} />} label="BW filter" />
+
+    return <FormControl size="small" sx={{ m: 1, minWidth: 120 }} >
+    <InputLabel id="filter-select">Image filter</InputLabel>
+    <Select
+      labelId="filter-select"
+      value={filter}
+      label="Image filter"
+      onChange={toggleFilter}
+    >
+      <MenuItem value="">
+        <em>None</em>
+      </MenuItem>
+      <MenuItem value="BlackWhite">Black & white</MenuItem>
+      <MenuItem value="Grayscale">Grayscale</MenuItem>
+    </Select>
+  </FormControl>
 }
 
 export default FilterDropdown;
