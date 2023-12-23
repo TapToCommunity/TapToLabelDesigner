37e5d49e039449cf0fc7d73e976b2725f2c488ae
Andrea Bogazzi <abogazzi@shutterstock.com>
2023-12-23 19:21:10 +0100

    template pcengine ok

--- src/components/TemplateDropdown.tsx
+++ src/components/TemplateDropdown.tsx
@@ -1,11 +1,12 @@
 import type { JSX, RefObject } from 'react';
 import { useCallback, useState } from 'react';
 import type { Canvas } from 'fabric';
-
+import { FabricImage, util, Point } from 'fabric';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { templates, cardLikeOptions } from '../constants';
 
 type FilterDropdownProps = {
   canvasArrayRef: RefObject<Canvas[]>;
@@ -18,9 +19,61 @@ const TemplateDropdown = ({ canvasArrayRef }: FilterDropdownProps): JSX.Element 
   const toggleTemplate = useCallback((evt: SelectChangeEvent<string>) => {
     const value = evt.target.value;
     const canvases = canvasArrayRef.current;
+    const template = templates[value];
     if (canvases) {
-      canvases.forEach((canvas) => {
-        canvas.requestRenderAll();
+      const { overlay, background } = template || {};
+      Promise.all([
+        overlay && util.loadImage(overlay.url),
+        background && util.loadImage(background.url),
+      ]).then(([overlayImageElement, backgroundImageElement]) => {
+        canvases.forEach((canvas) => {
+          const mainImage = canvas.getObjects('image')[0];
+          if (overlayImageElement) {
+            const scale = util.findScaleToCover(overlayImageElement, cardLikeOptions);
+            const overlayImg = new FabricImage(overlayImageElement, {
+              canvas,
+              scaleX: scale,
+              scaleY: scale,
+            });
+            canvas.overlayImage = overlayImg;
+            canvas.viewportCenterObject(overlayImg);
+            const pictureScale = util.findScaleToCover(mainImage, {
+              width: cardLikeOptions.width * overlay!.width,
+              height: cardLikeOptions.height * overlay!.height,
+            });
+            mainImage.set({
+              scaleX: pictureScale,
+              scaleY: pictureScale,
+            })
+            const templatePostion = overlayImg.translateToGivenOrigin(overlayImg.getRelativeXY(), 'center', 'center', 'left', 'top');
+            mainImage.setPositionByOrigin(new Point(
+              cardLikeOptions.width * overlay!.x + templatePostion.x,
+              cardLikeOptions.height * overlay!.y + templatePostion.y,
+            ), 'left', 'top');
+          } else {
+            // reset to BLANK
+            canvas.overlayImage = undefined;
+            const pictureScale = util.findScaleToCover(mainImage, cardLikeOptions);
+            mainImage.set({
+              scaleX: pictureScale,
+              scaleY: pictureScale,
+            })
+            mainImage.setPositionByOrigin(new Point(0, 0), 'left', 'top');
+          }
+          if (backgroundImageElement) {
+            const scale = util.findScaleToCover(backgroundImageElement, cardLikeOptions);
+            const backgroundImg = new FabricImage(backgroundImageElement, {
+              canvas,
+              scaleX: scale,
+              scaleY: scale,
+            });
+            canvas.backgroundImage = backgroundImg;
+            canvas.viewportCenterObject(backgroundImg);
+          } else {
+            canvas.backgroundImage = canvas.clipPath;
+          }
+          canvas.requestRenderAll();
+        });
       });
       setCurrentTemplate(value);
     }
@@ -38,8 +91,7 @@ const TemplateDropdown = ({ canvasArrayRef }: FilterDropdownProps): JSX.Element 
       <MenuItem value="blank">
         <em>Blank</em>
       </MenuItem>
-      <MenuItem value="BlackWhite">Black & white</MenuItem>
-      <MenuItem value="Grayscale">Grayscale</MenuItem>
+      {Object.entries(templates).map(([key, value]) => <MenuItem value={key}>{value.label}</MenuItem>)}
     </Select>
   </FormControl>
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
+import taptoOverlayImg from './assets/tapto_label.png';
+
 export const cardLikeOptions = {
   width: 854,
   height: 539,
@@ -9,3 +11,34 @@ export const cardLikeOptions = {
 };
 
 export const cardRatio = 855 / 540;
+
+type templateLayer = {
+  url: string;
+  /* percentage width where the overlaye transparent area begins */
+  x: number;
+  /* percentage height where the overlaye transparent area begins */
+  y: number;
+  /* percentage width that is transparent */
+  width: number;
+  /* percentage height that is transparent */
+  height: number;
+};
+
+type templateType = {
+  overlay?: templateLayer
+  background?: templateLayer;
+  label: string;
+}
+
+export const templates: Record<string, templateType> = {
+  tapto: {
+    overlay: {
+      url: taptoOverlayImg,
+      width: 702 / 975,
+      height: 547 / 600,
+      x: 26 / 975,
+      y: 27 / 600,
+    },
+    label: 'PC-Engine',
+  }
+} as const;
