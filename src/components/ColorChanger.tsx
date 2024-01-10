898821d4fe4f4881557bc8b0a9860f9775007009
Asturur <andreabogazzi79@gmail.com>
2024-01-10 03:29:56 +0100

    add reset button

--- src/components/ColorChanger.tsx
+++ src/components/ColorChanger.tsx
@@ -1,17 +1,30 @@
+import { IconButton } from '@mui/material';
 import { useAppDataContext } from '../contexts/appData';
 import { ColorButton } from './ColorButton';
+import RestartAltIcon from '@mui/icons-material/RestartAlt';
 
 export const ColorChanger = () => {
   const { originalColors, customColors, setCustomColors } = useAppDataContext();
-  return originalColors.map((color, index) => (
-    <ColorButton
-      key={color}
-      color={customColors[index]}
-      onClick={(color) => {
-        const newColors = [...customColors];
-        newColors[index] = color;
-        setCustomColors(newColors);
-      }}
-    />
-  ));
+  return (
+    <>
+      <IconButton
+        onClick={() => {
+          setCustomColors(originalColors);
+        }}
+      >
+        <RestartAltIcon />
+      </IconButton>
+      {originalColors.map((color, index) => (
+        <ColorButton
+          key={color}
+          color={customColors[index]}
+          onClick={(color) => {
+            const newColors = [...customColors];
+            newColors[index] = color;
+            setCustomColors(newColors);
+          }}
+        />
+      ))}
+    </>
+  );
 };
