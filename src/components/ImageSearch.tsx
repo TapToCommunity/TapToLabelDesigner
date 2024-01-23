9e58b803dc3976651d68d9c08dedbe39981344b6
Callan Barrett <callan@zoocar.org>
2024-01-23 21:50:02 +0800

    Add image fetching

--- src/components/ImageSearch.tsx
+++ src/components/ImageSearch.tsx
@@ -5,8 +5,11 @@ import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import { useState } from 'react';
+import { useFileDropperContext } from '../contexts/fileDropper';
+import { CircularProgress, LinearProgress, Stack } from '@mui/material';
 
-const API_URL = 'https://tapto.wizzo.dev/steamgriddb/api/search/';
+const SEARCH_ENDPOINT = 'https://tapto.wizzo.dev/steamgriddb/api/search/';
+const IMAGE_ENDPOINT = 'https://tapto.wizzo.dev/steamgriddb/api/image/';
 
 interface ImageSearchResult {
   gameName: string;
@@ -15,7 +18,7 @@ interface ImageSearchResult {
 }
 
 async function searchImage(query: string): Promise<ImageSearchResult[]> {
-  return fetch(API_URL + encodeURIComponent(query))
+  return fetch(SEARCH_ENDPOINT + encodeURIComponent(query))
     .then((res) => {
       return res.json() as Promise<ImageSearchResult[]>;
     })
@@ -25,12 +28,34 @@ async function searchImage(query: string): Promise<ImageSearchResult[]> {
     });
 }
 
+async function getImage(cdnUrl: string): Promise<File> {
+  const proxyUrl = IMAGE_ENDPOINT + cdnUrl.replace('https://', '');
+  return fetch(proxyUrl).then(async (r) => {
+    const blob = await r.blob();
+    const filename = proxyUrl.split('/').pop() || 'image.png';
+    return new File([blob], filename, { type: blob.type });
+  });
+}
+
 export function ImageSearch(props: {
   open: boolean;
   setOpen: (open: boolean) => void;
 }) {
+  const { files, setFiles } = useFileDropperContext();
+
   const [searchQuery, setSearchQuery] = useState<string>('');
   const [searchResults, setSearchResults] = useState<ImageSearchResult[]>([]);
+  const [searching, setSearching] = useState<boolean>(false);
+  const [fetching, setFetching] = useState<boolean>(false);
+
+  const addImage = async (url: string) => {
+    setFetching(true);
+    getImage(url).then((file) => {
+      setFiles([...files, file]);
+      props.setOpen(false);
+      setFetching(false);
+    });
+  };
 
   return (
     <Modal open={props.open} onClose={() => props.setOpen(false)}>
@@ -41,42 +66,69 @@ export function ImageSearch(props: {
           left: '50%',
           transform: 'translate(-50%, -50%)',
           width: '90%',
-          maxHeight: '90%',
+          height: !fetching ? '70%' : "15%",
           bgcolor: 'background.paper',
           border: '2px solid #000',
           boxShadow: 24,
           p: 4,
           color: 'text.primary',
           overflow: 'scroll',
+          borderRadius: '10px',
+          textAlign: 'center',
         }}
       >
-        <TextField
-          label="Search"
-          value={searchQuery}
-          size="small"
-          onChange={(evt) => setSearchQuery(evt.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            searchImage(searchQuery).then((res) => {
-              setSearchResults(res);
-            });
-          }}
-        >
-          Search
-        </Button>
-        <Typography variant="h3">
-          {searchResults.length > 0 ? searchResults[0].gameName : 'No Results'}
-        </Typography>
-        <Grid container spacing={1}>
-        {searchResults.map((result) => (
-          <Grid item xs={3}>
-            <img src={result.thumbnailUrl} />
-          </Grid>
-        ))}
-        </Grid>
-        
+        {fetching ? (
+          <>
+            <Typography variant="h3">Fetching image...</Typography>
+            <LinearProgress />
+          </>
+        ) : (
+          <>
+            <Stack spacing={1}>
+              <Stack
+                direction="row"
+                spacing="10px"
+                sx={{ justifyContent: 'center' }}
+              >
+                <TextField
+                  label="Game name"
+                  value={searchQuery}
+                  onChange={(evt) => setSearchQuery(evt.target.value)}
+                  style={{ width: '30%' }}
+                />
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setSearchResults([]);
+                    setSearching(true);
+                    searchImage(searchQuery).then((res) => {
+                      setSearching(false);
+                      setSearchResults(res);
+                    });
+                  }}
+                >
+                  {searching ? <CircularProgress color='secondary' /> : 'Search'}
+                </Button>
+              </Stack>
+              <Typography variant="h3">
+                {searchResults.length > 0
+                  ? searchResults[0].gameName
+                  : ''}
+              </Typography>
+              <Grid container spacing={1}>
+                {searchResults.map((result) => (
+                  <Grid item xs={3}>
+                    <img
+                      src={result.thumbnailUrl}
+                      onClick={() => addImage(result.imageUrl)}
+                      style={{ cursor: 'pointer' }}
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Stack>
+          </>
+        )}
       </Box>
     </Modal>
   );
