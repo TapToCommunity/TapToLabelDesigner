61c15605c8f2f6ccc63fcffd7a8b3945fceb70f3
Asturur <andreabogazzi79@gmail.com>
2024-01-25 12:14:09 +0100

    removed unused plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import ssl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ssl()],
   optimizeDeps: {
     exclude: ['fabric']
   },
-  server: {
-    https: true,
-  }
 });
