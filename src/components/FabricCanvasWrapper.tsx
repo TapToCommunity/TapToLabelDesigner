33d2c522117f612eed80936b624153a066268730
Asturur <andreabogazzi79@gmail.com>
2024-01-26 11:41:14 +0100

    cool stuff

--- src/components/FabricCanvasWrapper.tsx
+++ src/components/FabricCanvasWrapper.tsx
@@ -1,19 +1,12 @@
 import { useRef, useEffect } from 'react';
 import { cardLikeOptions } from '../constants';
-import {
-  StaticCanvas,
-  FabricImage,
-  util,
-  Rect,
-  FabricObject,
-  type Canvas,
-} from 'fabric';
+import { StaticCanvas, Rect, FabricObject, type Canvas } from 'fabric';
+
 type WrapperProp = {
-  file: File;
   setFabricCanvas: (canvas: StaticCanvas | null) => void;
 };
 
-export const FabricCanvasWrapper = ({ file, setFabricCanvas }: WrapperProp) => {
+export const FabricCanvasWrapper = ({ setFabricCanvas }: WrapperProp) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
@@ -23,31 +16,21 @@ export const FabricCanvasWrapper = ({ file, setFabricCanvas }: WrapperProp) => {
       const fabricCanvas = new StaticCanvas(canvasRef.current!, {
         width: cardLikeOptions.width,
         height: cardLikeOptions.height,
+        renderOnAddRemove: false,
       });
       const cardBorder = new Rect(cardLikeOptions);
       cardBorder.canvas = fabricCanvas as Canvas;
       fabricCanvas.clipPath = cardBorder;
       fabricCanvas.backgroundImage = cardBorder;
       fabricCanvas.centerObject(cardBorder);
-      const imageUrl = URL.createObjectURL(file);
-      if (file) {
-        util.loadImage(imageUrl).then((image) => {
-          const fabricImage = new FabricImage(image);
-          const scale = util.findScaleToCover(fabricImage, fabricCanvas);
-          fabricImage.scaleX = scale;
-          fabricImage.scaleY = scale;
-          fabricCanvas.add(fabricImage);
-          fabricCanvas.centerObject(fabricImage);
-          setFabricCanvas(fabricCanvas);
-        });
-      }
+      setFabricCanvas(fabricCanvas);
       return () => {
         if (fabricCanvas) {
           fabricCanvas.dispose();
         }
       };
     }
-  }, [setFabricCanvas, file]);
+  }, [setFabricCanvas]);
 
-  return <canvas ref={canvasRef} key={`${file.name}`} />;
+  return <canvas ref={canvasRef} />;
 };
--- src/hooks/useLabelEditor.ts
+++ src/hooks/useLabelEditor.ts
@@ -3,21 +3,24 @@ import { colorsDiffer } from '../utils/utils';
 import { updateColors } from '../utils/updateColors';
 import { useFileDropperContext } from '../contexts/fileDropper';
 import { scaleImageToOverlayArea, setTemplateOnCanvases } from '../utils/setTemplate';
-import type { FabricImage, StaticCanvas } from 'fabric';
+import { util, FabricImage, type StaticCanvas } from 'fabric';
 import { useAppDataContext } from '../contexts/appData';
 
 type useLabelEditorParams = {
   canvasArrayRef: MutableRefObject<StaticCanvas[]>;
   padderRef: MutableRefObject<HTMLDivElement | null>;
   index: number;
+  file: File | HTMLImageElement;
 };
 
-export const useLabelEditor = ({ canvasArrayRef, index, padderRef }: useLabelEditorParams) => {
+export const useLabelEditor = ({ canvasArrayRef, index, padderRef, file }: useLabelEditorParams) => {
   const { setFiles, files } = useFileDropperContext();
   const { template, customColors, originalColors, isIdle } =
     useAppDataContext();
   const [fabricCanvas, setFabricCanvas] = useState<StaticCanvas | null>(null);
-  const [ready, setReady] = useState<boolean>(false);
+  // local ready state, when template is loaded
+  const [fullyReady, setFullyReady] = useState<boolean>(false);
+  const [isImageReady, setImageReady] = useState<boolean>(false);
   const [localColors, setLocalColors] = useState<string[]>(customColors);
 
   const deleteLabel = useCallback(() => {
@@ -30,18 +33,44 @@ export const useLabelEditor = ({ canvasArrayRef, index, padderRef }: useLabelEdi
   }, [canvasArrayRef, files, index, setFiles]);
 
   const rotateMainImage = useCallback(() => {
-    if (ready && isIdle && fabricCanvas) {
-      const mainImage = fabricCanvas.getObjects()[0] as FabricImage;
+    if (fullyReady && isIdle && fabricCanvas) {
+      const mainImage = fabricCanvas.getObjects('image')[0] as FabricImage;
       mainImage.angle += 90;
       mainImage.angle %= 360;
       scaleImageToOverlayArea(template, fabricCanvas.overlayImage!, mainImage);
       fabricCanvas.requestRenderAll();
     }
-  }, [ready, isIdle, fabricCanvas, template])
+  }, [fullyReady, isIdle, fabricCanvas, template])
+
+  useEffect(() => {
+    if (fabricCanvas) {
+
+      const imagePromise =
+        file instanceof Blob
+          ? util.loadImage(URL.createObjectURL(file))
+          : Promise.resolve(file);
+      if (file) {
+        const currentImage = fabricCanvas.getObjects('image')[0];
+        if (currentImage) {
+          fabricCanvas.remove(currentImage);
+        }
+        setImageReady(false);
+        imagePromise.then((image) => {
+          const fabricImage = new FabricImage(image);
+          const scale = util.findScaleToCover(fabricImage, fabricCanvas);
+          fabricImage.scaleX = scale;
+          fabricImage.scaleY = scale;
+          fabricCanvas.add(fabricImage);
+          fabricCanvas.centerObject(fabricImage);
+          setImageReady(true);
+        });
+      }
+    }
+  }, [file, fabricCanvas]);
 
   useEffect(() => {
     const divRef = padderRef.current;
-    if (fabricCanvas && divRef) {
+    if (fabricCanvas && divRef && isImageReady) {
       if (canvasArrayRef.current) {
         fabricCanvas.setDimensions(
           { width: '100%' as unknown as number, height: 'auto' as unknown as number},
@@ -50,16 +79,18 @@ export const useLabelEditor = ({ canvasArrayRef, index, padderRef }: useLabelEdi
         canvasArrayRef.current[index] = fabricCanvas;
       }
       setTemplateOnCanvases([fabricCanvas], template).then((colors) => {
-        setReady(true);
+        setFullyReady(true);
         if (colorsDiffer(colors, localColors)) {
           setLocalColors(colors);
         }
+        fabricCanvas.requestRenderAll();
       });
     }
     // shouldn't retrigger for index change or template change or colors
     // the data reconciler does that
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [canvasArrayRef, fabricCanvas]);
+  }, [canvasArrayRef, fabricCanvas, isImageReady]);
+
 
   useEffect(() => {
     // every time customColors change reset the local
@@ -81,6 +112,7 @@ export const useLabelEditor = ({ canvasArrayRef, index, padderRef }: useLabelEdi
   }, [localColors, fabricCanvas, originalColors, isIdle]);
 
   return {
+    fabricCanvas,
     localColors,
     setLocalColors,
     deleteLabel,
