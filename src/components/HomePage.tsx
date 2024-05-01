437cf3673e0ef81b698b46f9a1d0cff9944dc176
Asturur <andreabogazzi79@gmail.com>
2024-05-01 08:18:12 +0200

    links in homepage

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -29,9 +29,25 @@ export const HomePage = () => {
             What’s TapTo?
           </Typography>
           <Typography>
-          <a href='https://github.com/wizzomafizzo/tapto/'>TapTo</a> is an open source system for launching games and scripted actions using physical objects like NFC cards. It's a great way to make playing games more accessible and add some fun to your gaming setup! TapTo is currently supported on <a href='https://mister-devel.github.io/MkDocs_MiSTer/'>MiSTer FPGA</a>, <a href='https://github.com/SensoriumEmbedded/TeensyROM/blob/main/docs/NFC_Loader.md'>Commodore 64 (via Teensy ROM)</a>, with other platforms coming soon!
+            <a href="https://github.com/wizzomafizzo/tapto/">TapTo</a> is an
+            open source system for launching games and scripted actions using
+            physical objects like NFC cards. It's a great way to make playing
+            games more accessible and add some fun to your gaming setup! TapTo
+            is currently supported on{' '}
+            <a href="https://mister-devel.github.io/MkDocs_MiSTer/">
+              MiSTer FPGA
+            </a>
+            ,{' '}
+            <a href="https://github.com/SensoriumEmbedded/TeensyROM/blob/main/docs/NFC_Loader.md">
+              Commodore 64 (via Teensy ROM)
+            </a>
+            , with other platforms coming soon!
           </Typography>
-          <Typography>Additional hardware is required but the aim is to be affordable and easily available. Please join the <a href='https://wizzo.dev/discord'>Discord</a> if you need any help or want to show off your work!
+          <Typography>
+            Additional hardware is required but the aim is to be affordable and
+            easily available. Please join the{' '}
+            <a href="https://wizzo.dev/discord">Discord</a> if you need any help
+            or want to show off your work!
           </Typography>
           <Typography className="links">
             <Link href="https://github.com/wizzomafizzo/tapto/releases">
@@ -66,21 +82,31 @@ export const HomePage = () => {
           <Typography variant="h3">What's TapTo Designer?</Typography>
           <img style={{ marginBottom: '18px' }} src={examplesUrl} />
           <Typography>
-          Having your favorite artwork on your NFC token is the perfect finishing touch for your TapTo collection. TapTo Designer streamlines this process to accomodate every skill level.
+            Having your favorite artwork on your NFC token is the perfect
+            finishing touch for your TapTo collection. TapTo Designer
+            streamlines this process to accomodate every skill level.
           </Typography>
           <Typography>
-          Simply upload your artwork, choose from a variety of TapTo label templates, and export in a growing number of print ready formats. Not sure where to get artwork? No worries! We have you covered with our integrated game search tools.
+            Simply upload your artwork, choose from a variety of TapTo label
+            templates, and export in a growing number of print ready formats.
+            Not sure where to get artwork? No worries! We have you covered with
+            our integrated game search tools.
           </Typography>
         </div>
       </div>
       <div className="credits">
-        <div className='textLayout'>
-        <Typography variant="h3">
-          Made with ❤️ by{' '}
-          <a href="https://github.com/asturur">Andrea Bogazzi</a>
-          <br />
-          Designed by <a href="https://timwilsie.com/">Tim Wilsie</a>
-        </Typography>
+        <div className="textLayout">
+          <Typography variant="h3">
+            Made with ❤️ by{' '}
+            <a href="https://github.com/asturur">Andrea Bogazzi</a>
+            <br />
+            Designed by <a href="https://timwilsie.com/">Tim Wilsie</a>
+            <br />
+            Templates provided by{' '}
+            <a href="https://www.artisticpixels305.com/">Ariel Aces</a>,{' '}
+            <a href="https://timwilsie.com/">Tim Wilsie</a>,{' '}
+            <a href="https://github.com/asturur">Andrea Bogazzi</a>
+          </Typography>
         </div>
       </div>
     </div>
