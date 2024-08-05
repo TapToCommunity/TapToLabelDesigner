409d7c1fe6a36c2633991a547efaed68865a0bba
Asturur <andreabogazzi79@gmail.com>
2024-08-05 08:43:37 +0200

    all

--- src/utils/processCustomizations.ts
+++ src/utils/processCustomizations.ts
@@ -2,32 +2,50 @@ import {
   type FabricObject,
   type Canvas,
   type Group,
+  FabricImage,
+  util,
 } from 'fabric';
 import type { templateType } from '../resourcesTypedef';
 
 
-const findById = (objects: (FabricObject | Group)[], id: string) => {
-  objects.forEach((object) => {
-    if(object.type === 'rect') {
-      console.log({ object, id })
-    }
+const findById = (objects: (FabricObject | Group)[], id: string): FabricObject | undefined => {
+  let foundObject: FabricObject | undefined;
+  for (const object of objects) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     if ((object as any).id === id) {
-      return object;
+      foundObject = object;
     }
     if ((object as Group).getObjects) {
-      findById((object as Group).getObjects(), id);
+      foundObject = findById((object as Group).getObjects(), id);
     }
-  })
+    if (foundObject) {
+      return foundObject;
+    }
+  }
 }
 
-export const processCustomizations = (canvas: Canvas, template: templateType) => {
+export const processCustomizations = async (canvas: Canvas, template: templateType) => {
   if (template.edits) {
     const { edits } = template;
     const objects = [...canvas.getObjects(), canvas.overlayImage, canvas.backgroundImage].filter((a) => !!a);
-    edits.forEach(({ id, resource }) => {
+    for (const { id, resource } of edits) {
       const fabricObject = findById(objects, id);
-      console.log({ fabricObject })
-    })
+      if (fabricObject) {
+        if (resource.type === 'image') {
+          // load first value
+          const image = await FabricImage.fromURL(resource.data[0].value);
+          const insertTarget = fabricObject.parent || canvas;
+          const insertObjects =  insertTarget.getObjects();
+          const index = insertObjects.indexOf(fabricObject);
+          insertTarget.insertAt(index + 1, image);
+          const scale = util.findScaleToFit(image, fabricObject);
+          const center = fabricObject.getRelativeCenterPoint();
+          image.scale(scale);
+          image.setPositionByOrigin(center, 'center', 'center');
+          insertTarget.set('dirty', true);
+          canvas.requestRenderAll();
+        }
+      }
+    }
   }
 }
