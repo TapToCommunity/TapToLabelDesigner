83de1598f8a6e99c33a5f9154c8b1d33b9a6b039
Asturur <andreabogazzi79@gmail.com>
2024-08-09 06:43:12 +0200

    working

--- src/components/SingleCardEditModal.tsx
+++ src/components/SingleCardEditModal.tsx
@@ -2,23 +2,38 @@ import Modal from '@mui/material/Modal';
 import './SingleCardEditModal.css';
 import Button from '@mui/material/Button';
 import { useFileDropperContext } from '../contexts/fileDropper';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Canvas } from 'fabric';
+import { useRealTimeResize } from '../hooks/useRealtimeResize';
 
-type SingleCardEditModalProps = {
-  isOpen: boolean;
+type SingleCardEditSpaceProps = {
   onClose: () => void;
   currentCardIndex: number;
 };
 
-export const SingleCardEditModal = ({
-  isOpen,
+type SingleCardEditModalProps = SingleCardEditSpaceProps & {
+  isOpen: boolean;
+};
+
+export const ModalInternalComponent = ({
   onClose,
   currentCardIndex,
-}: SingleCardEditModalProps) => {
+}: SingleCardEditSpaceProps) => {
   const { cards } = useFileDropperContext();
-  const editableCanvas = useRef<Canvas>();
+  const [ready, setReady] = useState(false);
+  const editableCanvas = useRef<Canvas | null>(null);
   const canvasElement = useRef<HTMLCanvasElement>(null);
+  const selectedCard = cards.current[currentCardIndex];
+  const padderRef = useRef<HTMLDivElement>(null);
+
+  useRealTimeResize({
+    fabricCanvas: editableCanvas.current,
+    layout: selectedCard.template!.layout,
+    ready,
+    padderRef,
+    throttleMs: 100,
+  });
+
   useEffect(() => {
     // mount, we duplicate a card
     if (currentCardIndex > -1 && canvasElement.current) {
@@ -30,25 +45,43 @@ export const SingleCardEditModal = ({
         const jsonData = selectedCard.canvas.toJSON();
         canvas.loadFromJSON(jsonData).then(() => {
           console.log('finished cloning template');
-          canvas.requestRenderAll();
+          setReady(true);
         });
       }
       return () => {
+        console.log('disposing');
         canvas && canvas.dispose();
       };
     }
   }, [cards, currentCardIndex]);
 
   return (
+    <>
+      <div className="verticalStack editSpace" ref={padderRef}>
+        <canvas key="doNotChangePlease" ref={canvasElement} />
+      </div>
+      <div className="horizontalStack">
+        <Button onClick={onClose}>Cancel</Button>
+        <Button onClick={onClose}>Confirm</Button>
+      </div>
+    </>
+  );
+};
+
+export const SingleCardEditModal = ({
+  isOpen,
+  onClose,
+  currentCardIndex,
+}: SingleCardEditModalProps) => {
+  return (
     <Modal open={isOpen} onClose={onClose}>
       <div className="cardEditModal verticalStack">
-        <div className="verticalStack editSpace">
-          <canvas key="doNotChangePlease" ref={canvasElement} />
-        </div>
-        <div className="horizontalStack">
-          <Button onClick={onClose}>Cancel</Button>
-          <Button onClick={onClose}>Confirm</Button>
-        </div>
+        {isOpen && (
+          <ModalInternalComponent
+            onClose={onClose}
+            currentCardIndex={currentCardIndex}
+          />
+        )}
       </div>
     </Modal>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -42,13 +42,11 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <AppDataContextProvider>
-        <FileDropperContextProvider>
-          <App />
-        </FileDropperContextProvider>
-      </AppDataContextProvider>
-    </ThemeProvider>
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <AppDataContextProvider>
+      <FileDropperContextProvider>
+        <App />
+      </FileDropperContextProvider>
+    </AppDataContextProvider>
+  </ThemeProvider>,
 );
